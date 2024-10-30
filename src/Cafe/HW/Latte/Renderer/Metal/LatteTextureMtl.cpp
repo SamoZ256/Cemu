25a63e9170f1a52a053e74b3ec5206fc7b43665a
Samuliak <samuliak77@gmail.com>
2024-10-30 09:31:03 +0100

    clean up texture creation code

--- src/Cafe/HW/Latte/Renderer/Metal/LatteTextureMtl.cpp
+++ src/Cafe/HW/Latte/Renderer/Metal/LatteTextureMtl.cpp
@@ -3,12 +3,35 @@
 #include "Cafe/HW/Latte/Renderer/Metal/MetalRenderer.h"
 #include "Cafe/HW/Latte/Renderer/Metal/LatteToMtl.h"
 #include "Common/precompiled.h"
-#include "Metal/MTLResource.hpp"
-#include "Metal/MTLTexture.hpp"
 
-LatteTextureMtl::LatteTextureMtl(class MetalRenderer* mtlRenderer, Latte::E_DIM dim, MPTR physAddress, MPTR physMipAddress, Latte::E_GX2SURFFMT format, uint32 width, uint32 height, uint32 depth, uint32 pitch, uint32 mipLevels, uint32 swizzle,
+LatteTextureMtl::LatteTextureMtl(class MetalRenderer* metalRenderer, Latte::E_DIM dim, MPTR physAddress, MPTR physMipAddress, Latte::E_GX2SURFFMT format, uint32 width, uint32 height, uint32 depth, uint32 pitch, uint32 mipLevels, uint32 swizzle,
 	Latte::E_HWTILEMODE tileMode, bool isDepth)
-	: LatteTexture(dim, physAddress, physMipAddress, format, width, height, depth, pitch, mipLevels, swizzle, tileMode, isDepth), m_mtlr(mtlRenderer), m_format(format), m_isDepth(isDepth)
+	: LatteTexture(dim, physAddress, physMipAddress, format, width, height, depth, pitch, mipLevels, swizzle, tileMode, isDepth), m_mtlr{metalRenderer}, m_format{format}, m_isDepth{isDepth}
+{
+    m_texture = CreateTexture(false);
+}
+
+LatteTextureMtl::~LatteTextureMtl()
+{
+	m_texture->release();
+}
+
+LatteTextureView* LatteTextureMtl::CreateView(Latte::E_DIM dim, Latte::E_GX2SURFFMT format, sint32 firstMip, sint32 mipCount, sint32 firstSlice, sint32 sliceCount)
+{
+	cemu_assert_debug(mipCount > 0);
+	cemu_assert_debug(sliceCount > 0);
+	cemu_assert_debug((firstMip + mipCount) <= this->mipLevels);
+	cemu_assert_debug((firstSlice + sliceCount) <= this->depth);
+
+	return new LatteTextureViewMtl(m_mtlr, this, dim, format, firstMip, mipCount, firstSlice, sliceCount);
+}
+
+void LatteTextureMtl::AllocateOnHost()
+{
+	cemuLog_log(LogType::MetalLogging, "not implemented");
+}
+
+MTL::Texture* LatteTextureMtl::CreateTexture(bool needsPixelFormatViewUsage)
 {
     MTL::TextureDescriptor* desc = MTL::TextureDescriptor::alloc()->init();
     desc->setStorageMode(MTL::StorageModePrivate);
@@ -60,50 +83,24 @@ LatteTextureMtl::LatteTextureMtl(class MetalRenderer* mtlRenderer, Latte::E_DIM 
     desc->setTextureType(textureType);
 
 	if (textureType == MTL::TextureType3D)
-	{
 		desc->setDepth(effectiveBaseDepth);
-	}
-	else if (textureType == MTL::TextureTypeCube)
-	{
-	    // Do nothing
-	}
 	else if (textureType == MTL::TextureTypeCubeArray)
-	{
 		desc->setArrayLength(effectiveBaseDepth / 6);
-	}
 	else
-	{
 		desc->setArrayLength(effectiveBaseDepth);
-	}
 
 	auto pixelFormat = GetMtlPixelFormat(format, isDepth);
 	desc->setPixelFormat(pixelFormat);
 
-	MTL::TextureUsage usage = MTL::TextureUsageShaderRead | MTL::TextureUsagePixelFormatView;
+	MTL::TextureUsage usage = MTL::TextureUsageShaderRead;
+	if (needsPixelFormatViewUsage)
+	    usage |= MTL::TextureUsagePixelFormatView;
 	if (!Latte::IsCompressedFormat(format))
 		usage |= MTL::TextureUsageRenderTarget;
 	desc->setUsage(usage);
 
-	m_texture = mtlRenderer->GetDevice()->newTexture(desc);
+	MTL::Texture* texture = m_mtlr->GetDevice()->newTexture(desc);
 	desc->release();
-}
 
-LatteTextureMtl::~LatteTextureMtl()
-{
-	m_texture->release();
-}
-
-LatteTextureView* LatteTextureMtl::CreateView(Latte::E_DIM dim, Latte::E_GX2SURFFMT format, sint32 firstMip, sint32 mipCount, sint32 firstSlice, sint32 sliceCount)
-{
-	cemu_assert_debug(mipCount > 0);
-	cemu_assert_debug(sliceCount > 0);
-	cemu_assert_debug((firstMip + mipCount) <= this->mipLevels);
-	cemu_assert_debug((firstSlice + sliceCount) <= this->depth);
-
-	return new LatteTextureViewMtl(m_mtlr, this, dim, format, firstMip, mipCount, firstSlice, sliceCount);
-}
-
-void LatteTextureMtl::AllocateOnHost()
-{
-	cemuLog_log(LogType::MetalLogging, "not implemented");
+	return texture;
 }
